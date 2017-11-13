91a18eaefea48d03a1dddaacc91f186eafc551c8
Ckjr <conan.nma@gmail.com>
2017-11-13 18:40:00 +0800

    Add dynamic earth

---
+++ JapanSavesTheWorld/Assets/Resources/Scripts/World/Spin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	private Quaternion rotation;
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround(Vector3.forward, Vector3.up, 10 * Time.deltaTime);
+	}
+}
