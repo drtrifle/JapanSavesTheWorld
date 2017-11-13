12e87ef92f628a8ec387c17a058d37e30b6127f6
Ckjr <conan.nma@gmail.com>
2017-11-13 19:20:02 +0800

    Added a sample (satellite) gameobject that orbits the world

---
+++ JapanSavesTheWorld/Assets/Resources/Scripts/World/Orbit2D.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit2D : MonoBehaviour {
+
+	[Header("References")]
+	[SerializeField]
+	private GameObject centerObject;
+
+	[Header("Settings")]
+	[SerializeField]
+	public float rotationSpeed = 10.0f;
+
+	private Vector3 desiredPosition;
+	private float radius = 2.0f;
+
+	void Start () {
+		radius = ((Vector2) this.transform.position - (Vector2) centerObject.transform.position).magnitude;
+	}
+
+	void Update () {
+		transform.RotateAround (this.transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+		transform.position = 
+			RotatePointAroundPivot(transform.position,
+				centerObject.transform.position,
+				Quaternion.Euler(0, 0, rotationSpeed * Time.deltaTime));
+	}
+
+	public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle) {
+		return angle * (point - pivot) + pivot;
+	}
+
+
+}
